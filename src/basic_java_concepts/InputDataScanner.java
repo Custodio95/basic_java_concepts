a03b89cf370eb3df6a97bd942a900e5d9f18ac7e
custodio95 <gabriel.custodio.ferreira@gmail.com>
2022-06-06 10:56:31 -0300

    Add Class InputDataScanner

---
+++ src/basic_java_concepts/InputDataScanner.java
@@ -0,0 +1,30 @@
+package basic_java_concepts;
+
+// import class Scanner
+import java.util.Scanner;
+
+public class InputDataScanner {
+
+	public static void main(String[] args) {
+		
+		// Class to test Method Scanner
+				
+		// Instance Object
+		Scanner sc = new Scanner(System.in);
+				
+		// How to use
+		System.out.println("Programa de Soma");
+		
+		System.out.println("Insira o Primeiro Valor:");
+		double valor1 = sc.nextDouble();
+		
+		System.out.println("Insira o Segundo Valor:");
+		double valor2 = sc.nextDouble();
+		
+		double res = valor1 + valor2;
+
+		System.out.println("O resultado é: " + res);
+		
+	}
+
+}
