a5dd1a64bcbfd94265a121e5ccbc175bbcd19615
custodio95 <gabriel.custodio.ferreira@gmail.com>
2022-06-09 13:59:34 -0300

    add class StructureFor

---
+++ src/basic_java_concepts/StructureFor.java
@@ -0,0 +1,30 @@
+package basic_java_concepts;
+
+import java.util.Scanner;
+
+// Class to test For
+
+/*Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+exemplo.*/
+
+public class StructureFor {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int n = sc.nextInt();
+		
+		for (int i=1;i<=n;i++) {
+			System.out.println(i+" "+i*i+" "+i*i*i);	
+		}
+		
+		
+		sc.close();
+		
+
+	}
+
+}
